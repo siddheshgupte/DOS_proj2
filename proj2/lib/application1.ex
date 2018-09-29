7a5d125da09554584d84b263b70b1061ba61554a
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-09-29 07:08:34 +0530

    Add full and line topologies

--- proj2/lib/application1.ex
+++ proj2/lib/application1.ex
@@ -3,7 +3,10 @@ defmodule Application1 do
 
   @gossip_limit 10
 
-  def start(_type, num_of_nodes) do
+# Start the application with the number of nodes
+  def start(_type, num_of_nodes, topology) do
+
+    # Create a list of children Supervisor.child_spec({Proj2, [10, :"Node 1"]},id: "Node 1")
     children =
       1..num_of_nodes
       |> Enum.to_list()
@@ -15,8 +18,28 @@ defmodule Application1 do
       end)
 
     opts = [strategy: :one_for_one, name: Supervisor]
+    
+    # Get the supervisor in the supervisor variable
     {:ok, supervisor} = Supervisor.start_link(children, opts)
 
-    Supervisor.which_children(supervisor)
+    # Get a list of children of the supervisor
+    lst = Supervisor.which_children(supervisor)
+        |> Enum.map(fn x -> elem(x, 0) end)
+
+    case topology do
+        "full" ->            
+            # For Fully connected network
+            # Set neighbours of the current node
+            Enum.each(lst, fn x -> GenServer.cast(x, {:set_neighbours, List.delete(lst, x)}) end)
+        "line" ->
+            1..(num_of_nodes-2)
+                |> Enum.to_list
+                |> Enum.each( fn x -> GenServer.cast(Enum.at(lst, x), {:set_neighbours, [Enum.at(lst, x-1), Enum.at(lst, x+1)]}) end)
+
+            GenServer.cast(:"Node 1", {:set_neighbours, [:"Node 2"]})
+            GenServer.cast(String.to_atom("Node #{num_of_nodes}"), {:set_neighbours, [String.to_atom("Node #{num_of_nodes - 1}")]})
+            
+    end
+    
   end
 end
--- proj2/lib/proj2.ex
+++ proj2/lib/proj2.ex
@@ -1,5 +1,6 @@
+import :timer
 defmodule Proj2 do
-  use GenServer
+  use GenServer, restart: :temporary
 
   # External API
   def start_link([gossip_limit, input_name]) do
@@ -21,8 +22,34 @@ defmodule Proj2 do
   end
 
   def handle_cast(:gossip, current_map) do
-    {_, updated_map} = Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+
     IO.puts current_map.current_gossip_count
+
+    if current_map.current_gossip_count <= 0 do
+      # GenServer.cast(self(), :kill_self)
+      Process.exit(self(), :normal)
+    end
+
+    GenServer.cast(Enum.random(current_map.neighbour_list), :gossip)
+
+    {_, updated_map} = Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+
+    sleep 100
+    GenServer.cast(self(), :gossip)
     {:noreply, updated_map}
+  end
+
+  def handle_cast({:set_neighbours, list_of_neighbours}, current_map) do
+    {_,updated_map} = Map.get_and_update(current_map, :neighbour_list, fn x -> {x, list_of_neighbours} end)
+    IO.inspect updated_map
+    {:noreply, updated_map}
+  end
+
+  def handle_cast(:kill_self, current_map) do
+    {:stop, :normal, current_map}
+  end
+
+  def terminate(_, current_map) do
+    IO.inspect "Dead"
   end
 end
