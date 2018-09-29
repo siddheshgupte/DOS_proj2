c12afba4ee6c802108d6127a8fca4eb179bb1c85
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-09-30 03:19:21 +0530

    Implement pushsum

--- proj2/lib/application1.ex
+++ proj2/lib/application1.ex
@@ -3,129 +3,169 @@ defmodule Application1 do
 
   @gossip_limit 10
   def connect_horizontally(ip_lst, step) do
-    0..length(ip_lst)-1
-        |> Enum.to_list
-        |> Enum.each(fn x ->
-              GenServer.cast(Enum.at(ip_lst, x), {:set_neighbours, Enum.filter(
-                  [(if x-step >=0, do: Enum.at(ip_lst, x-step), else: nil),
-                   Enum.at(ip_lst, x+step)], fn x -> x != nil end)}) end)
-        if((step-1) > 0) do
-            connect_horizontally(ip_lst,step-1)
-        end
-    end    
+    0..(length(ip_lst) - 1)
+    |> Enum.to_list()
+    |> Enum.each(fn x ->
+      GenServer.cast(
+        Enum.at(ip_lst, x),
+        {:set_neighbours,
+         Enum.filter(
+           [
+             if(x - step >= 0, do: Enum.at(ip_lst, x - step), else: nil),
+             Enum.at(ip_lst, x + step)
+           ],
+           fn x -> x != nil end
+         )}
+      )
+    end)
 
-    def connect_vertically(list_of_lists,step) do
-        0..length(list_of_lists)-1
-        |> Enum.to_list
-        |> Enum.each(fn x ->
-            0..length(list_of_lists)-1
-            |> Enum.to_list
-             |> Enum.each(fn y ->
-              GenServer.cast(Enum.at(Enum.at(list_of_lists, x),y), {:set_neighbours, 
-              Enum.filter(
-                #   [Enum.at(Enum.at(list_of_lists, x-step),y), Enum.at(Enum.at(list_of_lists,x+step), y)], 
-                [(if x-step >= 0, do: Enum.at(Enum.at(list_of_lists, x-step),y), else: nil),
-                 (if x+step < length(list_of_lists), do: Enum.at(Enum.at(list_of_lists,x+step), y), else: nil)],    
-                 fn x -> x != nil end)
-                  }) end)
-             end)
-
-        if((step-1) > 0) do
-            connect_vertically(list_of_lists,step-1)
-        end
-
+    if(step - 1 > 0) do
+      connect_horizontally(ip_lst, step - 1)
     end
-      
-    #for torus
-    def connect_horizontally(ip_lst) do
-        0..length(ip_lst)-1
-            |> Enum.to_list
-            |> Enum.each(fn x ->
-                  GenServer.cast(Enum.at(ip_lst, x),
-                  {:set_neighbours,
-                   Enum.filter([Enum.at(ip_lst, x-1), Enum.at(ip_lst, rem(x+1,length(ip_lst)) )], fn x -> x != nil end)} ) end)
-    end    
-    
-    def connect_vertically(list_of_lists) do
-            0..length(list_of_lists)-1
-            |> Enum.to_list
-            |> Enum.each(fn x ->
-                0..length(list_of_lists)-1
-                |> Enum.to_list
-                 |> Enum.each(fn y ->
-                  GenServer.cast(Enum.at(Enum.at(list_of_lists, x),y), {:set_neighbours, 
-                    #   [Enum.at(Enum.at(list_of_lists, x-step),y), Enum.at(Enum.at(list_of_lists,x+step), y)], 
-                    [Enum.at(Enum.at(list_of_lists, x-1),y),
-                      Enum.at(Enum.at(list_of_lists,rem(x+1,length(list_of_lists)) ), y)]    
-                      }) end)
-                 end)
-        end
-    
-   
+  end
 
-# Start the application with the number of nodes
+  def connect_vertically(list_of_lists, step) do
+    0..(length(list_of_lists) - 1)
+    |> Enum.to_list()
+    |> Enum.each(fn x ->
+      0..(length(list_of_lists) - 1)
+      |> Enum.to_list()
+      |> Enum.each(fn y ->
+        GenServer.cast(Enum.at(Enum.at(list_of_lists, x), y), {:set_neighbours,
+         Enum.filter(
+           #   [Enum.at(Enum.at(list_of_lists, x-step),y), Enum.at(Enum.at(list_of_lists,x+step), y)], 
+           [
+             if(x - step >= 0, do: Enum.at(Enum.at(list_of_lists, x - step), y), else: nil),
+             if(x + step < length(list_of_lists),
+               do: Enum.at(Enum.at(list_of_lists, x + step), y),
+               else: nil
+             )
+           ],
+           fn x -> x != nil end
+         )})
+      end)
+    end)
+
+    if(step - 1 > 0) do
+      connect_vertically(list_of_lists, step - 1)
+    end
+  end
+
+  # for torus
+  def connect_horizontally(ip_lst) do
+    0..(length(ip_lst) - 1)
+    |> Enum.to_list()
+    |> Enum.each(fn x ->
+      GenServer.cast(
+        Enum.at(ip_lst, x),
+        {:set_neighbours,
+         Enum.filter(
+           [Enum.at(ip_lst, x - 1), Enum.at(ip_lst, rem(x + 1, length(ip_lst)))],
+           fn x -> x != nil end
+         )}
+      )
+    end)
+  end
+
+  def connect_vertically(list_of_lists) do
+    0..(length(list_of_lists) - 1)
+    |> Enum.to_list()
+    |> Enum.each(fn x ->
+      0..(length(list_of_lists) - 1)
+      |> Enum.to_list()
+      |> Enum.each(fn y ->
+        GenServer.cast(Enum.at(Enum.at(list_of_lists, x), y), {
+          :set_neighbours,
+          #   [Enum.at(Enum.at(list_of_lists, x-step),y), Enum.at(Enum.at(list_of_lists,x+step), y)], 
+          [
+            Enum.at(Enum.at(list_of_lists, x - 1), y),
+            Enum.at(Enum.at(list_of_lists, rem(x + 1, length(list_of_lists))), y)
+          ]
+        })
+      end)
+    end)
+  end
+
+  # Start the application with the number of nodes
   def start(_type, num_of_nodes, topology) do
-
     # Create a list of children Supervisor.child_spec({Proj2, [10, :"Node 1"]},id: "Node 1")
     children =
       1..num_of_nodes
       |> Enum.to_list()
       |> Enum.map(fn x ->
         Supervisor.child_spec(
-          {Proj2, [@gossip_limit, String.to_atom("Node #{x}")]},
+          {Proj2, [@gossip_limit, String.to_atom("Node #{x}"), x]},
           id: String.to_atom("Node #{x}")
         )
       end)
 
     opts = [strategy: :one_for_one, name: Supervisor]
-    
+
     # Get the supervisor in the supervisor variable
     {:ok, supervisor} = Supervisor.start_link(children, opts)
 
     # Get a list of children of the supervisor
-    lst = Supervisor.which_children(supervisor)
-        |> Enum.map(fn x -> elem(x, 0) end)
-     #   |>Enum.sort
+    lst =
+      Supervisor.which_children(supervisor)
+      |> Enum.map(fn x -> elem(x, 0) end)
+
+    #   |>Enum.sort
 
     case topology do
-        "full" ->            
-            # For Fully connected network
-            # Set neighbours of the current node
-            Enum.each(lst, fn x -> GenServer.cast(x, {:set_neighbours, List.delete(lst, x)}) end)
-        "line" ->
-            1..(num_of_nodes-2)
-                |> Enum.to_list
-                |> Enum.each( fn x -> GenServer.cast(Enum.at(lst, x), {:set_neighbours, [Enum.at(lst, x-1), Enum.at(lst, x+1)]}) end)
+      "full" ->
+        # For Fully connected network
+        # Set neighbours of the current node
+        Enum.each(lst, fn x -> GenServer.cast(x, {:set_neighbours, List.delete(lst, x)}) end)
 
-            GenServer.cast(:"Node 1", {:set_neighbours, [:"Node 2"]})
-            GenServer.cast(String.to_atom("Node #{num_of_nodes}"), {:set_neighbours, [String.to_atom("Node #{num_of_nodes - 1}")]})
-        "imp2D" ->
-            1..(num_of_nodes-2)
-                |> Enum.to_list
-                |> Enum.each( fn x -> GenServer.cast(Enum.at(lst, x), {:set_neighbours, [Enum.at(lst, x-1), Enum.at(lst, x+1), Enum.random(lst)]}) end)
+      "line" ->
+        1..(num_of_nodes - 2)
+        |> Enum.to_list()
+        |> Enum.each(fn x ->
+          GenServer.cast(
+            Enum.at(lst, x),
+            {:set_neighbours, [Enum.at(lst, x - 1), Enum.at(lst, x + 1)]}
+          )
+        end)
 
-                GenServer.cast(:"Node 1", {:set_neighbours, [:"Node 2", Enum.random(lst)]})
-                GenServer.cast(String.to_atom("Node #{num_of_nodes}"), {:set_neighbours, [String.to_atom("Node #{num_of_nodes - 1}"), Enum.random(lst)]})
-        "rand2D" ->
-            len = trunc(:math.sqrt(num_of_nodes))
-            step = trunc(len/10)
-            if(step !=0) do
-                list_of_lists = Enum.chunk_every(lst, len)   
-                Enum.each(list_of_lists, fn x -> connect_horizontally(x, step) end)
-                connect_vertically(list_of_lists,step)
-            end
-        
-        "torus" ->
-            len = trunc(:math.sqrt(num_of_nodes))
-            list_of_lists = Enum.chunk_every(lst, len)   
-            Enum.each(list_of_lists, fn x -> connect_horizontally(x) end)
-            connect_vertically(list_of_lists)
+        GenServer.cast(:"Node 1", {:set_neighbours, [:"Node 2"]})
 
+        GenServer.cast(
+          String.to_atom("Node #{num_of_nodes}"),
+          {:set_neighbours, [String.to_atom("Node #{num_of_nodes - 1}")]}
+        )
 
+      "imp2D" ->
+        1..(num_of_nodes - 2)
+        |> Enum.to_list()
+        |> Enum.each(fn x ->
+          GenServer.cast(
+            Enum.at(lst, x),
+            {:set_neighbours, [Enum.at(lst, x - 1), Enum.at(lst, x + 1), Enum.random(lst)]}
+          )
+        end)
 
-    
+        GenServer.cast(:"Node 1", {:set_neighbours, [:"Node 2", Enum.random(lst)]})
+
+        GenServer.cast(
+          String.to_atom("Node #{num_of_nodes}"),
+          {:set_neighbours, [String.to_atom("Node #{num_of_nodes - 1}"), Enum.random(lst)]}
+        )
+
+      "rand2D" ->
+        len = trunc(:math.sqrt(num_of_nodes))
+        step = trunc(len / 10)
+
+        if(step != 0) do
+          list_of_lists = Enum.chunk_every(lst, len)
+          Enum.each(list_of_lists, fn x -> connect_horizontally(x, step) end)
+          connect_vertically(list_of_lists, step)
+        end
+
+      "torus" ->
+        len = trunc(:math.sqrt(num_of_nodes))
+        list_of_lists = Enum.chunk_every(lst, len)
+        Enum.each(list_of_lists, fn x -> connect_horizontally(x) end)
+        connect_vertically(list_of_lists)
     end
-    
-    end
-    
+  end
 end
--- proj2/lib/proj2.ex
+++ proj2/lib/proj2.ex
@@ -1,12 +1,19 @@
 import :timer
+
 defmodule Proj2 do
   use GenServer, restart: :temporary
 
   # External API
-  def start_link([gossip_limit, input_name]) do
+  def start_link([gossip_limit, input_name, node_index]) do
     GenServer.start_link(
       __MODULE__,
-      %{:current_gossip_count => gossip_limit, :neighbour_list => []},
+      %{
+        :current_gossip_count => gossip_limit,
+        :neighbour_list => [],
+        :s => node_index,
+        :w => 1,
+        :current_pushsum_count => 3
+      },
       name: input_name
     )
   end
@@ -17,13 +24,14 @@ defmodule Proj2 do
   end
 
   def handle_call(:gossip, _from, current_map) do
-    {_, updated_map} = Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+    {_, updated_map} =
+      Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+
     {:reply, current_map, updated_map}
   end
 
   def handle_cast(:gossip, current_map) do
-
-    IO.puts current_map.current_gossip_count
+    IO.puts(current_map.current_gossip_count)
 
     if current_map.current_gossip_count <= 0 do
       # GenServer.cast(self(), :kill_self)
@@ -32,16 +40,62 @@ defmodule Proj2 do
 
     GenServer.cast(Enum.random(current_map.neighbour_list), :gossip)
 
-    {_, updated_map} = Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+    {_, updated_map} =
+      Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
 
-    sleep 100
+    sleep(100)
     GenServer.cast(self(), :gossip)
     {:noreply, updated_map}
   end
 
+  def handle_cast({:pushsum, [input_s, input_w, is_self]}, current_map) do
+    #  Calculate previous ratio
+    prev_ratio = current_map[:s] / current_map[:w]
+
+    # Update s and w values
+    {_, current_map} = Map.get_and_update(current_map, :s, fn x -> {x, x + input_s} end)
+    {_, current_map} = Map.get_and_update(current_map, :w, fn x -> {x, x + input_w} end)
+
+    #  Calculate current ratio
+    current_ratio = current_map[:s] / current_map[:w]
+
+    {_, current_map} =
+      if not is_self do
+        #  Check for convergence
+        if current_ratio - prev_ratio < 10.0e-10 do
+          Map.get_and_update(current_map, :current_pushsum_count, fn x -> {x, x - 1} end)
+        else
+          {"abc", current_map}
+        end
+      else
+        {"abc", current_map}
+      end
+
+    # Keep half of s and w
+    {_, current_map} = Map.get_and_update(current_map, :s, fn x -> {x, x / 2} end)
+    {_, current_map} = Map.get_and_update(current_map, :w, fn x -> {x, x / 2} end)
+
+    GenServer.cast(
+      Enum.random(current_map.neighbour_list),
+      {:pushsum, [current_map[:s], current_map[:w], false]}
+    )
+
+    #  If converged, exit
+    if current_map[:current_pushsum_count] <= 0 do
+      IO.inspect("Dead #{current_map[:s] / current_map[:w]}")
+      Process.exit(self(), :normal)
+    end
+
+    sleep(500)
+    GenServer.cast(self(), {:pushsum, [0.0, 0.0, true]})
+    {:noreply, current_map}
+  end
+
   def handle_cast({:set_neighbours, list_of_neighbours}, current_map) do
-    {_,updated_map} = Map.get_and_update(current_map, :neighbour_list, fn x -> {x, x ++ list_of_neighbours} end)
-    IO.inspect updated_map
+    {_, updated_map} =
+      Map.get_and_update(current_map, :neighbour_list, fn x -> {x, x ++ list_of_neighbours} end)
+
+    IO.inspect(updated_map)
     {:noreply, updated_map}
   end
 
@@ -50,6 +104,6 @@ defmodule Proj2 do
   end
 
   def terminate(_, current_map) do
-    IO.inspect "Dead"
+    IO.inspect("Dead")
   end
 end
